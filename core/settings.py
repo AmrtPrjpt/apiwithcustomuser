72c7082184cb3d1c7f1ba6e17e9c820f840258a9
AmrtPrjpt <amrtprjpt@gmail.com>
2022-05-25 10:30:10 +0545

    settings

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,6 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = "users.User"
